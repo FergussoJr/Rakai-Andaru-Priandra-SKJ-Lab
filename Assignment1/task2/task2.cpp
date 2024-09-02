ca110e72a50f4bf2207ddeb80574a2f4f8b87551
FergussoJr <120631884+FergussoJr@users.noreply.github.com>
2024-09-02 16:21:33 +0700

    Add files via upload

---
+++ Assignment1/task2/task2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1 = 5;
+    int num2 = 10;
+    int result;
+
+ 
+    result = num1 * num2;
+
+
+   cout << "Result: " << result << endl;
+
+    return 0; 
+}
